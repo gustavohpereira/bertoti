c2bc169f85fec892d0b0519fd2c052f414361f33
Gustavo Henrique <108089562+gustavohpereira@users.noreply.github.com>
2023-05-19 08:12:16 -0300

    bertoti update

--- projetoSlide23/src/App.java
+++ projetoSlide23/src/App.java
@@ -1,5 +1,50 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Estoque estoque = new Estoque();
+        
+        // Criando alguns jogos de exemplo
+        Game game1 = new Game();
+        game1.setName("The Legend of Zelda: Breath of the Wild");
+        game1.setConsole("Nintendo Switch");
+        game1.setValor(59.99);
+        
+        Game game2 = new Game();
+        game2.setName("Super Mario Odyssey");
+        game2.setConsole("Nintendo Switch");
+        game2.setValor(49.99);
+        
+        Game game3 = new Game();
+        game3.setName("God of War");
+        game3.setConsole("PlayStation 4");
+        game3.setValor(39.99);
+        
+        // Cadastrando os jogos no estoque
+        estoque.cadastrarGame(game1);
+        estoque.cadastrarGame(game2);
+        estoque.cadastrarGame(game3);
+        
+        // Testando a funcionalidade de buscar por nome
+        List<Game> jogosPorNome = estoque.buscarPorNome("Super Mario Odyssey");
+        System.out.println("Jogos encontrados por nome:");
+        for (Game jogo : jogosPorNome) {
+            System.out.println(jogo.getName() + " - " + jogo.getConsole() + " - R$" + jogo.getValor());
+        }
+        System.out.println();
+        
+        // Testando a funcionalidade de buscar por console
+        List<Game> jogosPorConsole = estoque.buscarPorConsole("Nintendo Switch");
+        System.out.println("Jogos encontrados por console:");
+        for (Game jogo : jogosPorConsole) {
+            System.out.println(jogo.getName() + " - " + jogo.getConsole() + " - R$" + jogo.getValor());
+        }
+        System.out.println();
+        
+        // Testando a funcionalidade de buscar por valor
+        List<Game> jogosPorValor = estoque.buscarPorValor(39.99);
+        System.out.println("Jogos encontrados por valor:");
+        for (Game jogo : jogosPorValor) {
+            System.out.println(jogo.getName() + " - " + jogo.getConsole() + " - R$" + jogo.getValor());
+        }
+        System.out.println();
     }
 }
--- projetoSlide23/src/Estoque.java
+++ projetoSlide23/src/Estoque.java
@@ -2,20 +2,45 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Estoque {
-    private List<Game> games = new LinkedList<Game>();
+    private List<Game> games = new ArrayList<>();
     
-    public void cadastrarGame(Game game){
-        games.add(game)
+    public void cadastrarGame(Game game) {
+        games.add(game);
     }
-    public List<Game> buscarPorName(String name){
-        for(Game game : games){
-            if (game.getName().equals(name)){
-            
+    
+    public List<Game> buscarPorNome(String nome) {
+        List<Game> resultados = new ArrayList<>();
+        
+        for (Game game : games) {
+            if (game.getName().equalsIgnoreCase(nome)) {
+                resultados.add(game);
             }
-            
         }
         
-
+        return resultados;
     }
-
+    
+    public List<Game> buscarPorConsole(String console) {
+        List<Game> resultados = new ArrayList<>();
+        
+        for (Game game : games) {
+            if (game.getConsole().equalsIgnoreCase(console)) {
+                resultados.add(game);
+            }
+        }
+        
+        return resultados;
+    }
+    
+    public List<Game> buscarPorValor(double valor) {
+        List<Game> resultados = new ArrayList<>();
+        
+        for (Game game : games) {
+            if (game.getValor() == valor) {
+                resultados.add(game);
+            }
+        }
+        
+        return resultados;
+    }
 }
